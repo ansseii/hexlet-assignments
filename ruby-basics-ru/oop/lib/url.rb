9d819c6c22a08e3d9dd61cafc6270ddf51c66a9e
@hexlet/cli <support@hexlet.io>
2023-03-20 22:44:00 +0100

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -5,13 +5,14 @@ require 'forwardable'
 require 'uri'
 
 class Url
-  extend Forwardable
   include Comparable
+  extend Forwardable
   def_delegators :@url, :scheme, :host, :port
 
   def initialize(url)
-    @url = URI(url)
-    @params = Hash[URI.decode_www_form(@url.query ||= '')].transform_keys(&:to_sym)
+    @url = URI.parse(url)
+    query = @url.query || ''
+    @params = Hash[URI.decode_www_form(query)].transform_keys(&:to_sym)
   end
 
   def query_params
@@ -22,9 +23,8 @@ class Url
     query_params.fetch key, default
   end
 
-  def <=>(other_url)
-    [@url.scheme, @url.host, @url.port.to_i, @params] <=>
-      [other_url.scheme, other_url.host, other_url.port.to_i, other_url.query_params]
+  def <=>(other)
+    [scheme, host, port, query_params] <=> [other.scheme, other.host, other.port, other.query_params]
   end
 end
 

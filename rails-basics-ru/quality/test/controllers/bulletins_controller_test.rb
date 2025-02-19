aad3c01bad5f227d3f4f7d685788108b1f01295b
@hexlet/cli <support@hexlet.io>
2025-02-19 14:01:47 +0100

    submit rails-basics-ru/quality

---
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class BulletinsTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @bulletin = bulletins(:one)
+  end
+
+  test 'should get index' do
+    get bulletins_url
+
+    assert_response :success
+  end
+
+  test 'should show bulletin' do
+    get bulletins_url @bulletin
+
+    assert_response :success
+  end
+end
